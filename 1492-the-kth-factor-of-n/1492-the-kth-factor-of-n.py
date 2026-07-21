90b67d1244e41ba8b34c446b3d3ba2302e2598d4
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-07-21 10:49:44 +0530

    Time: 5 ms (11%), Space: 12.4 MB (17.87%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def kthFactor(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        
+        factor=1
+        for i in range(1,n+1):
+            if n%i==0 and k:
+                factor=i
+                k-=1
+        if k==0:
+            return factor
+        return -1
+        
