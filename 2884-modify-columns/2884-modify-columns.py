2c29efbf0479b5a196e67592243b7e22ebfab79e
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-07-22 08:55:46 +0530

    Time: 265 ms (73.29%), Space: 66 MB (89.22%) - LeetHub

--- 2884-modify-columns/2884-modify-columns.py
+++ 2884-modify-columns/2884-modify-columns.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    sal=employees['salary']*2
+    
     return employees.assign(salary=2*employees['salary'])
     # return employees
