0be525c6423d6243aa9153e850d3fed478380435
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-07-30 11:03:58 +0530

    Time: 11 ms (32.96%), Space: 21.1 MB (48.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         freq={}
+        max_=0
         for i , n in enumerate(nums):
             if n in freq:
                 freq[n]+=1
+                if freq[n]>len(nums)/2:
+                    return n
             else:
+                if len(nums)==1:
+                    return n
                 freq[n]=1
-        for n,f in freq.items():
-            if f >len(nums)/2:
-                return n
+        # for n,f in freq.items():
+        #     if f >len(nums)/2:
+        #         return n
