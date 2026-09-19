5d9057982a77f372bce2ffe278514f93140b6b2d
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-09-19 11:54:45 +0530

    Time: 0 ms (100%), Space: 21 MB (70.53%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,16 +1,7 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # target TC O(n) SC O(1)
-        unique={}
-        for n in nums:
-            if n not in unique:
-                unique[n]=1
-            else:
-                unique[n]+=1
-        for n,c in unique.items():
-            if c==1:
-                return n
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        # solving o(n) and o(1)
+        xor=nums[0]
+        for i in range(1,len(nums)):
+            xor=xor^nums[i]
+        return xor
