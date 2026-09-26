5d21655421c83efedc1773ef0c0d70f6cd411a9c
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-09-27 01:06:28 +0530

    Time: 53 ms (51.04%), Space: 51.5 MB (62.09%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def evaluate(self, s, knowledge):
-        """
-        :type s: str
-        :type knowledge: List[List[str]]
-        :rtype: str
-        """
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
         _map={}
         for pair in knowledge:
             _map[pair[0]]=pair[1]
